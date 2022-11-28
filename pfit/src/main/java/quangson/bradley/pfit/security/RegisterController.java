f524d2fd07964f25b7b9f75491aba9aa91a3cf91
Mauvecrow <64569782+mauvecrow@users.noreply.github.com>
2022-11-28 12:12:15 -0700

    basic Register bean and jsf to create new users Users are stored in the Identity Store db TODO: update create logic to automatically add planner role

---
+++ pfit/src/main/java/quangson/bradley/pfit/security/RegisterController.java
@@ -0,0 +1,84 @@
+package quangson.bradley.pfit.security;
+
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+import jakarta.security.enterprise.identitystore.Pbkdf2PasswordHash;
+import quangson.bradley.pfit.security.isd.ISDCaller;
+import quangson.bradley.pfit.security.isd.ISDCallerService;
+
+@Named("cRegister")
+@RequestScoped
+public class RegisterController {
+
+    @Inject
+    ISDCallerService service;
+
+    @Inject
+    Pbkdf2PasswordHash passwordHash;
+
+    //ISDCaller fields
+    private String username;
+    private String password;
+    private String email;
+    private String firstName;
+    private String lastName;
+
+    private ISDCaller buildUser(){
+        ISDCaller user = new ISDCaller(
+                username,
+                passwordHash.generate(password.toCharArray()),
+                email,
+                firstName,
+                lastName
+        );
+        return user;
+    }
+
+
+    public String register(){
+        var user = buildUser();
+        service.addUser(user);
+        return "login";
+    }
+    // getters and setters
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
