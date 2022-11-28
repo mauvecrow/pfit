557bbc53162066b7eb9f66323de6e5966b17d477
Mauvecrow <64569782+mauvecrow@users.noreply.github.com>
2022-11-28 10:37:32 -0700

    Declare configurations to enable a database identity storage. Based on Payara example

---
+++ pfit/src/main/java/quangson/bradley/pfit/ApplicationConfig.java
@@ -0,0 +1,23 @@
+package quangson.bradley.pfit;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.security.enterprise.authentication.mechanism.http.CustomFormAuthenticationMechanismDefinition;
+import jakarta.security.enterprise.authentication.mechanism.http.LoginToContinue;
+import jakarta.security.enterprise.identitystore.DatabaseIdentityStoreDefinition;
+
+@ApplicationScoped
+@DatabaseIdentityStoreDefinition(
+        dataSourceLookup = "java:/MySqlDS",
+        callerQuery = "select enc_password from id_store_callers where username = ?",
+        groupsQuery = "select idsg.group from id_store_groups idsg, id_store_callers idsc " +
+                "where idsg.caller_id = idsc.ids_caller_id and idsc.username = ?"
+)
+@CustomFormAuthenticationMechanismDefinition(
+        loginToContinue = @LoginToContinue(
+                loginPage = "/login.xhtml",
+                errorPage = "",
+                useForwardToLogin = false
+        )
+)
+public class ApplicationConfig {
+}
