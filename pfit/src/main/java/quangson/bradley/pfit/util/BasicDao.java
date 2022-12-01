743dbe3e608fe1e57c8c30fe780d66e30dd0af67
Mauvecrow <64569782+mauvecrow@users.noreply.github.com>
2022-12-01 13:11:41 -0700

    Update the BasicDao interface to include 2 new methods: findOneByNamedQuery - added to leverage named queries, this is specific to any named query designed to return 1 object based on some criteria findMany - added to get a List<T> of entities. JpaBasicDao leverages the Criteria API to do this

--- pfit/src/main/java/quangson/bradley/pfit/util/BasicDao.java
+++ pfit/src/main/java/quangson/bradley/pfit/util/BasicDao.java
@@ -1,9 +1,15 @@
 package quangson.bradley.pfit.util;
 
+import java.util.List;
+
 public interface BasicDao<T> {
 
     void create(T entity);
     T read(int basicId);
     int update(T entity);
     void delete(T entity);
+
+    T findOneByNamedQuery(String namedQuery, String paramName, String paramVal);
+    List<T> findMany();
+
 }
--- pfit/src/main/java/quangson/bradley/pfit/util/JpaBasicDao.java
+++ pfit/src/main/java/quangson/bradley/pfit/util/JpaBasicDao.java
@@ -2,6 +2,12 @@ package quangson.bradley.pfit.util;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
+
+import java.util.List;
 
 public abstract class JpaBasicDao<T> implements BasicDao<T>{
 
@@ -31,5 +37,22 @@ public abstract class JpaBasicDao<T> implements BasicDao<T>{
     @Override
     public void delete(T entity) {
         em.remove(em.merge(entity));
+    }
+
+    @Override
+    public T findOneByNamedQuery(String namedQuery, String paramName, String paramVal){
+        return em.createNamedQuery(namedQuery, assignClass())
+                .setParameter(paramName, paramVal)
+                .getSingleResult();
+    }
+
+    @Override
+    public List<T> findMany() {
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<T> q = cb.createQuery(assignClass());
+        Root<T> root = q.from(assignClass());
+        q.select(root);
+        TypedQuery<T> tq = em.createQuery(q);
+        return tq.getResultList();
     }
 }
