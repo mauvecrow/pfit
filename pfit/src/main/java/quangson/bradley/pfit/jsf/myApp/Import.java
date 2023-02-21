f4141e0b43d618738fe130c82aab262d0f00f20d
Mauvecrow <64569782+mauvecrow@users.noreply.github.com>
2023-02-20 19:04:19 -0700

    Added import feature to load transactions. Process requires reading a csv file with header. Header columns: Date,Source,Vendor,Amount,Notes Date format is: M/d/yyyy

--- pfit/src/main/java/quangson/bradley/pfit/jsf/myApp/Import.java
+++ pfit/src/main/java/quangson/bradley/pfit/jsf/myApp/Import.java
@@ -1,28 +1,56 @@
 package quangson.bradley.pfit.jsf.myApp;
 
+import jakarta.ejb.EJB;
+import jakarta.faces.context.FacesContext;
 import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Inject;
 import jakarta.inject.Named;
 import jakarta.servlet.http.Part;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import quangson.bradley.pfit.transaction.ejb.TrxManager;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.Serializable;
+import java.io.*;
 import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 @Named("cImport")
 @ViewScoped
 public class Import implements Serializable {
 
+    private static final Logger logger = LogManager.getLogger(Import.class);
+
+    @EJB
+    private TrxManager trxManager;
+
+    @Inject
+    FacesContext facesContext;
+
     private Part uploadedFile;
-    private String filename;
-    private byte[] fileContent;
 
     public void upload(){
-        filename = Paths.get(uploadedFile.getSubmittedFileName())
-                .getFileName()
-                .toString();
-        try (InputStream input = uploadedFile.getInputStream()){
-            fileContent = input.readAllBytes();
+//        String filename = Paths.get(uploadedFile.getSubmittedFileName())
+//                .getFileName()
+//                .toString();
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(uploadedFile.getInputStream()))){
+            String headerRow = br.readLine(); //read first line which is the header row
+            String line;
+          while((line = br.readLine()) != null){
+//              logger.info(line);
+              String[] rows = line.split(",");
+              // expected structure: Date,Source,Vendor,Amount,Notes
+              var newTrx = trxManager.startTrxBuild()
+                      .date(LocalDate.parse(rows[0], DateTimeFormatter.ofPattern("M/d/yyyy")))
+                      .source(rows[1])
+                      .vendor(rows[2])
+                      .amount(Double.parseDouble(rows[3]))
+                      .notes(rows[4])
+                      .owner(facesContext.getExternalContext().getUserPrincipal().getName())
+                      .build();
+//              logger.info(newTrx);
+              trxManager.addTransaction(newTrx);
+          }
         }
         catch (IOException ioe){
             // show faces message
@@ -39,19 +67,4 @@ public class Import implements Serializable {
         this.uploadedFile = uploadedFile;
     }
 
-    public String getFilename() {
-        return filename;
-    }
-
-    public void setFilename(String filename) {
-        this.filename = filename;
-    }
-
-    public byte[] getFileContent() {
-        return fileContent;
-    }
-
-    public void setFileContent(byte[] fileContent) {
-        this.fileContent = fileContent;
-    }
 }
