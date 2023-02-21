cd108a20981c00ac1cfb027d37a6639690209174
Mauvecrow <64569782+mauvecrow@users.noreply.github.com>
2023-02-20 19:06:09 -0700

    Updated to include a TrxBuilder inner class and new method to create Transaction

--- pfit/src/main/java/quangson/bradley/pfit/transaction/ejb/TrxManager.java
+++ pfit/src/main/java/quangson/bradley/pfit/transaction/ejb/TrxManager.java
@@ -15,6 +15,7 @@ public class TrxManager implements Serializable {
     @Inject
     TransactionDao dao;
 
+    TrxBuilder trxBuilder;
     public void addTransaction(Transaction newTrx){
         dao.create(newTrx);
     }
@@ -25,6 +26,71 @@ public class TrxManager implements Serializable {
 
     public List<Transaction> getRecentMonthlyTrx(String username, long offset){
         return dao.getRecentTransactions(username, LocalDate.now().minusMonths(offset) );
+    }
+
+    public TrxBuilder startTrxBuild(){
+        if(trxBuilder == null){
+            trxBuilder = new TrxBuilder();
+        }
+        return trxBuilder;
+    }
+
+
+
+    public static class TrxBuilder {
+        private LocalDate trxDate;
+        private String trxSource;
+        private String trxVendor;
+        private double trxAmt;
+        private String notes = null;
+        private String trxOwner = null;
+        private TrxBuilder(){
+            trxDate = LocalDate.now();
+            trxSource = "Manual entry";
+            trxVendor = "unknown";
+            trxAmt = 0.0;
+        }
+
+        public TrxBuilder date(LocalDate date){
+            this.trxDate = date;
+            return this;
+        }
+
+        public TrxBuilder source(String source){
+            this.trxSource = source;
+            return this;
+        }
+
+        public TrxBuilder vendor(String vendor){
+            this.trxVendor = vendor;
+            return this;
+        }
+
+        public TrxBuilder amount(double amount){
+            this.trxAmt = amount;
+            return this;
+        }
+
+        public TrxBuilder notes(String notes){
+            this.notes = notes;
+            return this;
+        }
+
+        public TrxBuilder owner(String owner){
+            this.trxOwner = owner;
+            return this;
+        }
+
+        public Transaction build(){
+            var newTrx = new Transaction();
+            newTrx.setTransactionDate(trxDate);
+            newTrx.setTransactionSource(trxSource);
+            newTrx.setVendor(trxVendor);
+            newTrx.setAmount(trxAmt);
+            newTrx.setNotes(notes);
+            newTrx.setTrxOwner(trxOwner);
+            return newTrx;
+        }
     }
 
     /*
