93c5146b8a5c0bad5553f1021c658c9f72fa08a4
Mauvecrow <64569782+mauvecrow@users.noreply.github.com>
2023-03-14 18:59:00 -0600

    Revert "Updated to use Date instead of LocalDate"
    This reverts commit 481a37013bfd1f41645ccd1899633f0b6a7bf3b5.

--- pfit/src/main/java/quangson/bradley/pfit/transaction/TransactionDao.java
+++ pfit/src/main/java/quangson/bradley/pfit/transaction/TransactionDao.java
@@ -2,11 +2,11 @@ package quangson.bradley.pfit.transaction;
 
 import quangson.bradley.pfit.util.BasicDao;
 
-import java.util.Date;
+import java.time.LocalDate;
 import java.util.List;
 
 public interface TransactionDao extends BasicDao<Transaction> {
 
     List<Transaction> getTransactions(String trxOwner);
-    List<Transaction> getRecentTransactions(String trxOwner, Date sinceDate);
+    List<Transaction> getRecentTransactions(String trxOwner, LocalDate sinceDate);
 }
