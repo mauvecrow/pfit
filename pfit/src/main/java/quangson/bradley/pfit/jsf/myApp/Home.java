c7bcce37e8dcc87e61691a86183adbcffcab6c13
Mauvecrow <64569782+mauvecrow@users.noreply.github.com>
2023-03-25 15:17:03 -0600

    Current working state. WIP with pages

--- pfit/src/main/java/quangson/bradley/pfit/jsf/myApp/Home.java
+++ pfit/src/main/java/quangson/bradley/pfit/jsf/myApp/Home.java
@@ -5,17 +5,21 @@ import jakarta.annotation.PostConstruct;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Inject;
 import jakarta.inject.Named;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import quangson.bradley.pfit.transaction.Transaction;
 import quangson.bradley.pfit.transaction.ejb.TrxManager;
 
 import java.util.List;
 
+
 @Named("cHome")
 @RequestScoped
 public class Home {
 
+    private static final Logger logger = LogManager.getLogger(Home.class);
     List<Transaction> recentTrx;
-    private long offset = 1;
+    private long offset = 6;
     @Inject
     private TrxManager trxManager;
 
